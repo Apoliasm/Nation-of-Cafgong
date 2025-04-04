baa330ac6a5b17acdb2b1314253f6a7fae2ffb74
Apoliasm <syjsir@gmail.com>
2025-04-04 17:14:46 +0900

    Fix Eslint options

--- noc-front/eslint.config.mjs
+++ noc-front/eslint.config.mjs
@@ -12,11 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "off",
+      "no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
+    extends: ["next/core-web-vitals", "next/typescript"],
   }),
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
