d2336f150f9d8c49bb5242a66ff7050d6f9f0226
Apoliasm <syjsir@gmail.com>
2025-09-16 17:12:04 +0900

    FEAT : Add Search place
    검색 -> 전역 상태 반영
    카카오맵 센터 이동
    검색 키워드 주변 카페에 마커 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,11 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { apiSrc } from "@/api";
-import SDKLoader from "@/lib/kakaoMap/SDKLoader";
+import KakaoMapCanvas from "@/components/KakaoMapCanvas";
 declare global {
   interface Window {
     kakao: typeof kakao;
   }
 }
 export default function Home() {
-  useEffect(() => {
-    SDKLoader()
-      .then(() => {
-        //sdk를 불러왔다면 promise의 fulfilled 함수로 지도를 dom에 집어넣기
-        if (window.kakao && window.kakao.maps) {
-          inputMapContainer();
-        } else {
-          console.error("Kakao maps not available");
-        }
-      })
-      .catch((e) => console.log(e));
-  }, []);
-
-  return (
-    <div>
-      <div className="w-full h-screen static" id="map"></div>
-    </div>
-  );
-
-  function inputMapContainer() {
-    window.kakao.maps.load(() => {
-      console.log("Kakao Maps Loaded");
-
-      const coords = new window.kakao.maps.LatLng(33.5563, 126.79581);
-      const container = document.getElementById("map");
-
-      if (container) {
-        const options = { center: coords, level: 3 };
-        const map = new window.kakao.maps.Map(container, options);
-        map.setCenter(coords);
-      } else {
-        console.error("Map container not found");
-      }
-    });
-  }
+  return <KakaoMapCanvas />;
 }
